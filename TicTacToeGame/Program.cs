83f3cc8657a0708aa3a1d994cdf259c4e7e0e5e0
Omkarbade <omakrbade3799@gmail.com>
2022-02-03 11:37:50 +0530

    [Omkar] Add TicTacToeGame program

---
+++ TicTacToeGame/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeGame
+{
+    internal class Program
+    { 
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to tic tac toe game");
+            TicTacToeGame game = new TicTacToeGame();
+            Console.WriteLine();
+        } 
+    }
+}
---
+++ TicTacToeGame/startgame.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeGame
+{
+    internal class TicTacToeGame
+    {
+        static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+    }
+}
